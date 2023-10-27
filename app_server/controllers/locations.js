0bf31240043e37167fa5c0329ce1502999f967e2
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2023-10-28 00:09:08 +0900

    Update locations.js

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,7 @@ const apiOptions = {
   server: 'http://localhost:3000'
 };
 if(process.env.NODE_ENV === 'production') {
-  apiOptions.server = 'https://loc8r-api-puwk.onrender.com';
+  apiOptions.server = 'https://loc8r-api-btiq.onrender.com';
 }
 
 const homelist = (req, res) => {
